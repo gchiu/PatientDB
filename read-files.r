a752c27c267fb16940984d4c391aa36ea33dcdc3
Graham Chiu <compkarori@gmail.com>
2021-10-31 18:13:49 +0000

    Update read-files.r

--- read-files.r
+++ read-files.r
@@ -10,7 +10,7 @@ filename-rule: [nhi-rule "-" some alpha "-202" 5 digit "-" digit ".txt"]
 dbase: open odbc://patients
 port: first dbase
 
-foreach file letters: read %patients [
+foreach file letters: read %patients/ [
     ; if the filename matches the filename-rule, then check to see if it is in the database
     ; if not, then add it
     ffile: form file
