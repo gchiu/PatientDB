50c110cc0d2418998f95756d6960d1023b12ebfa
Graham Chiu <compkarori@gmail.com>
2021-10-31 18:10:03 +0000

    Update read-files.r

--- read-files.r
+++ read-files.r
@@ -3,7 +3,7 @@ Rebol []
 ; read all the files into the files database
 
 digit: charset [#"0"-#"9"]
-alpha: charset [#"a"-"z" #"A"-"Z"]
+alpha: charset [#"a"-"z" #"A"-#"Z"]
 nhi-rule: [3 alpha 4 digit]
 filename-rule: [nhi-rule "-" some alpha "-202" 5 digit "-" digit ".txt"]
 
@@ -11,7 +11,7 @@ dbase: open odbc://patients
 port: first dbase
 
 foreach file letters: read %patients [
-    ; if the filename matches the filename-rule, then check to see if it's in the database
+    ; if the filename matches the filename-rule, then check to see if it is in the database
     ; if not, then add it
     ffile: form file
     if parse ffile filename-rule [
