61e002ce88aaff708a986e21bd4988eedbae0e58
Graham Chiu <compkarori@gmail.com>
2021-11-01 08:54:47 +0000

    Update parse-letter.r

--- parse-letter.r
+++ parse-letter.r
@@ -6,6 +6,8 @@ Rebol [
 dbase: open odbc://patients
 port: first dbase
 
+dir: %patients/
+
 ; get all the clinicians first
 insert port {select id, surname from clinicians}
 clinicians: copy []
@@ -27,7 +29,7 @@ foreach record copy port [
     filename: record/2
     nhi: uppercase copy/part filename 7
     current-doc: none
-    if parse filename [skip 7 "-" copy clinician some alpha thru "-" copy ldate 8 digit "-" to ".txt" end][
+    if parse filename [NHI "-" copy clinician some alpha thru "-" copy ldate 8 digit "-" to ".TXT" to end][
         ; GChiu, Elasir
         foreach [doc id] clinicians [
             if find clinician doc [
@@ -41,9 +43,12 @@ foreach record copy port [
             ldate: to date! rejoin [day "-" month "-" year]
             print reform ["clinician id is " current-doc]
             print reform ["clinic letter date is " ldate]
+            ; now read the letter to parse the contents
+            contents: read join dir filename
+            probe contents
+            halt
         ][
             ; no doc found, skip this letter
-
         ]
     ]
 ]
