4df1a70a8ad5aa77ae5e754a1356b87e20dbf946
Graham Chiu <compkarori@gmail.com>
2021-11-01 08:41:35 +0000

    Update parse-letter.r

--- parse-letter.r
+++ parse-letter.r
@@ -6,12 +6,44 @@ Rebol [
 dbase: open odbc://patients
 port: first dbase
 
+; get all the clinicians first
+insert port {select id, surname from clinicians}
+clinicians: copy []
+foreach c copy port [
+    append clinicians reduce [c/2 c/1]
+]
+; Chiu 1 Elasir 2
+probe clinicians
+
+
+digit: charset [#"0"- #"9"]
+alpha: charset [#"a" - #"z" #"A" - #"Z"]
+nhi-rule: [3 alpha 4 digit]
+filename-rule: [nhi-rule "-" some alpha "-202" 5 digit "-" digit ".txt"]
+
 insert port [{select id, filename from files where done = (?)} false]
 foreach record copy port [
     fileid: record/1
     filename: record/2
-    nhi: copy/part filename 7
-    parse filename 
+    nhi: uppercase copy/part filename 7
+    current-doc: none
+    if parse filename [skip 7 "-" copy clinician some alpha thru "-" copy ldate 8 digit "-" to ".txt" end][
+        ; GChiu, Elasir
+        foreach [doc id] clinicians [
+            if find clinician doc [
+                current-doc: id
+                break
+            ]
+        ]
+        either current-doc [
+            ; convert ldate to a proper date
+            parse ldate [copy year 4 digit copy month 2 digit copy day 2 digit]
+            ldate: to date! rejoin [day "-" month "-" year]
+            print reform ["clinician id is " current-doc]
+            print reform ["clinic letter date is " ldate]
+        ][
+            ; no doc found, skip this letter
 
-
+        ]
+    ]
 ]
