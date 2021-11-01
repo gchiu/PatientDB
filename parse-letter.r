192c372379a1b3e5d92c66c585cab6f92e21ea1c
Graham Chiu <compkarori@gmail.com>
2021-11-01 19:23:15 +0000

    Update parse-letter.r

--- parse-letter.r
+++ parse-letter.r
@@ -19,7 +19,9 @@ probe clinicians
 
 
 digit: charset [#"0"- #"9"]
+dob-rule: [2 digit "." 2 digit "." 4 digit]
 alpha: charset [#"a" - #"z" #"A" - #"Z"]
+uc: charset [#"A" - #"Z"]
 nhi-rule: [3 alpha 4 digit]
 filename-rule: [nhi-rule "-" some alpha "-202" 5 digit "-" digit ".txt"]
 months: ["January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December"]
@@ -51,16 +53,64 @@ foreach record copy port [
             ck: checksum/secure contents
             lines: deline/lines contents ; split into lines and parse each line
             header: name: dob: false
+            address: copy []
+            mode: 'date ;'
             foreach line lines [
                 trim/head/tail line
                 if not empty? line [
-                    if find line longdate [
-                        ; now we are in the header
-                        header: true
-                    ]
-                    if header [
-                        ; need a state machine here
+                    switch mode [
+                        'date [ ;'
+                            if find line longdate [
+                                ; now we are in the header
+                                mode: 'name ;'
+                            ]
+                        ]
 
+                        'name [ ;'look for patient name next eg. XXXX, XXXX XXXX 
+                            surname: fname: sname: none
+                            if parse line [uc some alpha ", " copy fname some alpha opt [" " copy sname to end] end ][
+                                ; we have surnames, and first names
+                                parse line [surname copy some alpha]
+                                surname: uppercase surname
+                                fname: uppercase fname
+                                if sname [sname: uppercase sname]
+                                mode: 'nhi ;'
+                            ]
+                        ]
+
+                        'nhi [;' confirm nhi matches that from the filename
+                            if parse line ["NHI: " copy letter-nhi nhi-rule][
+                                either letter-nhi <> nhi [
+                                    print "Mismatch on file NHI and Letter NHI"
+                                    break
+                                ][
+                                    mode: 'address ;'
+                                ]
+                            ]
+                        ]
+
+                        'address [;' start capturing address lines and dob mixed in together, terminated by finding GP:
+                            case [
+                                parse line ["DOB: " copy dob dob-rule][
+                                    replace/all dob "." "-"
+                                    dob: to date! dob
+                                ]
+
+                                parse line ["GP:" copy fp to end][
+                                    mode: 'fp ;' got the FP name
+                                ]
+
+                                parse line ["M: " copy mobile to end]
+
+                                parse line ["P" opt "h" ": " copy phone to end]
+                                
+                                find line "@" [email: copy line]
+
+                                true [; just address lines
+                                    append/only address line
+                                ]
+                            ]
+                        ]
                     ]
                 ]
             ]
