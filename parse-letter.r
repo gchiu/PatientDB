a814081a84d98e86df4d5a19f3c69cc7eba91626
Graham Chiu <compkarori@gmail.com>
2021-11-02 19:14:22 +0000

    Update parse-letter.r

--- parse-letter.r
+++ parse-letter.r
@@ -85,191 +85,191 @@ foreach record copy port [
 				; now check to see if the letters database has this letter or not
 				insert port [{select id from letters where checksum = (?)} ck]
 				either none? pick port 1 [; okay not done yet
-				mode: 'date ;' we look for the date first to start the processing
-				foreach line deline/lines contents [; split into lines and parse each line
-					trim/head/tail line
-					either not empty? line [
-						switch mode [
-							date [
-								if find line longdate [
-									; now we are in the header
-									mode: 'name ;'
+					mode: 'date ;' we look for the date first to start the processing
+					foreach line deline/lines contents [; split into lines and parse each line
+						trim/head/tail line
+						either not empty? line [
+							switch mode [
+								date [
+									if find line longdate [
+										; now we are in the header
+										mode: 'name ;'
+									]
 								]
-							]
 
-							name [;look for patient name next eg. XXXX, XXXX XXXX or XXX XXX, XXX XXX
-								?? line
-								either parse/all line [uc some name-rule ", " copy fname fname-rule opt [" " copy sname to end] end] [
-									; we have surnames, and first names
-									parse/all line [copy surname to ","]
-									?? surname ?? fname ?? sname
-									surname: uppercase surname
-									fname: uppercase fname
-									if sname [sname: uppercase sname]
-									mode: 'nhi ;'
-								] [
-									print ["can't find name in line " line]
-									halt
-								]
-							]
-
-							nhi [; confirm nhi matches that from the filename
-								if parse line ["NHI: " copy letter-nhi nhi-rule] [
-									either letter-nhi <> nhi [
-										print "Mismatch on file NHI and Letter NHI"
-										break
+								name [;look for patient name next eg. XXXX, XXXX XXXX or XXX XXX, XXX XXX
+									?? line
+									either parse/all line [uc some name-rule ", " copy fname fname-rule opt [" " copy sname to end] end] [
+										; we have surnames, and first names
+										parse/all line [copy surname to ","]
+										?? surname ?? fname ?? sname
+										surname: uppercase surname
+										fname: uppercase fname
+										if sname [sname: uppercase sname]
+										mode: 'nhi ;'
 									] [
-										mode: 'address ;'
+										print ["can't find name in line " line]
+										halt
 									]
 								]
-							]
 
-							address [; start capturing address lines and dob mixed in together, terminated by finding GP:
-								case [
-									parse line ["DOB: " copy dob dob-rule] [
-										replace/all dob "." "-"
-										dob: to date! dob
-									]
-
-									parse/all line ["GP: " copy fp to end] [
-										fpname: last parse fp none
-										mode: 'fp ;' got the FP name
-									]
-
-									parse/all line [some [phone-rule | mobile-rule | space] end] []
-
-									find line "@" [email: copy line]
-
-									true [; just address lines
-										; get area code out
-										rline: reverse copy line
-										if parse/all rline [copy areacode areacode-rule space copy line to end] [
-											areacode: reverse areacode
-											line: reverse line
+								nhi [; confirm nhi matches that from the filename
+									if parse line ["NHI: " copy letter-nhi nhi-rule] [
+										either letter-nhi <> nhi [
+											print "Mismatch on file NHI and Letter NHI"
+											break
+										] [
+											mode: 'address ;'
 										]
-										append/only address line
 									]
 								]
-							]
 
-							fp [; extract fp address
-								case [
-									find/part line "Dear" 4 [
-										mode: 'end-salutation ;'
+								address [; start capturing address lines and dob mixed in together, terminated by finding GP:
+									case [
+										parse line ["DOB: " copy dob dob-rule] [
+											replace/all dob "." "-"
+											dob: to date! dob
+										]
+
+										parse/all line ["GP: " copy fp to end] [
+											fpname: last parse fp none
+											mode: 'fp ;' got the FP name
+										]
+
+										parse/all line [some [phone-rule | mobile-rule | space] end] []
+
+										find line "@" [email: copy line]
+
+										true [; just address lines
+											; get area code out
+											rline: reverse copy line
+											if parse/all rline [copy areacode areacode-rule space copy line to end] [
+												areacode: reverse areacode
+												line: reverse line
+											]
+											append/only address line
+										]
 									]
+								]
 
-									find/part line "INTERNAL" 8 [
-										; internal referral
+								fp [; extract fp address
+									case [
+										find/part line "Dear" 4 [
+											mode: 'end-salutation ;'
+										]
+
+										find/part line "INTERNAL" 8 [
+											; internal referral
+											mode: 'finish ;'
+										]
+
+										true [
+											if not find line fpname [
+												; if there are tabs in the line, it's from a copy to someone else
+												; eg {Kauri HealthCare^-^-^-^Whanganui Hospital} ;'
+												if find line #"^-" [
+													parse line [copy line to #"^-"]
+												]
+												append fpaddress line
+											]
+										]
+
+									]
+								]
+
+								end-salutation [
+									if find/part line "Diagnos" 7 [
+										mode: 'diagnosis ;'
+									]
+									if find/part line "INTERNAL" 8 [
+										print "internal referral"
 										mode: 'finish ;'
 									]
+								]
 
-									true [
-										if not find line fpname [
-											; if there are tabs in the line, it's from a copy to someone else
-											; eg {Kauri HealthCare^-^-^-^Whanganui Hospital} ;'
-											if find line #"^-" [
-												parse line [copy line to #"^-"]
+								diagnosis [
+									either find line "Medicat" [
+										mode: 'medication ;'
+									] [
+										; check to see if leading number eg. 1. or -, the former to be removed and the latter indicates details
+										; 1. 	Psoriatic Arthritis
+										; Anti-CCP +ve rheumatoid arthritis 
+										?? line
+										case [
+											parse/all line [any whitespace "-" any whitespace copy dline to end |
+												any whitespace alpha ")" any whitespace copy dline to end ; a), b)^-
+											] [
+												if dline [
+													trim/head/tail dline
+													append diagnosis-detail join dline newline
+												]
 											]
-											append fpaddress line
+											parse/all line [
+												some digit "." any whitespace copy line to end | ; where the diagnosis starts with a digit
+												copy line some diagnosis-rule to end
+											] [
+												; submode: 'gotdx ;'
+												trim/head/tail line
+												if not empty? diagnosis-detail [
+													append/only diagnoses reduce [trim/tail diagnosis-detail]
+													diagnosis-detail: copy ""
+												]
+												append diagnoses line
+											]
 										]
+										; append diagnoses line
 									]
-
 								]
-							]
 
-							end-salutation [
-								if find/part line "Diagnos" 7 [
-									mode: 'diagnosis ;'
-								]
-								if find/part line "INTERNAL" 8 [
-									print "internal referral"
-									mode: 'finish ;'
-								]
-							]
-
-							diagnosis [
-								either find line "Medicat" [
-									mode: 'medication ;'
-								] [
-									; check to see if leading number eg. 1. or -, the former to be removed and the latter indicates details
-									; 1. 	Psoriatic Arthritis
-									; Anti-CCP +ve rheumatoid arthritis 
+								medication [
 									?? line
-									case [
-										parse/all line [any whitespace "-" any whitespace copy dline to end |
-											any whitespace alpha ")" any whitespace copy dline to end ; a), b)^-
-										] [
-											if dline [
-												trim/head/tail dline
-												append diagnosis-detail join dline newline
-											]
-										]
-										parse/all line [
-											some digit "." any whitespace copy line to end | ; where the diagnosis starts with a digit
-											copy line some diagnosis-rule to end
-										] [
-											; submode: 'gotdx ;'
-											trim/head/tail line
-											if not empty? diagnosis-detail [
-												append/only diagnoses reduce [trim/tail diagnosis-detail]
-												diagnosis-detail: copy ""
-											]
-											append diagnoses line
-										]
+									either find line "DMARDS" [
+										mode: 'dmards ;'
+									] [
+										append medications line
 									]
-									; append diagnoses line
+								]
+
+								dmards [
+									append dmards line
+								]
+
+								finish [
+									print "break out of lines as no diagnosis or medications in this letter"
+									break
 								]
 							]
 
-							medication [
-								?? line
-								either find line "DMARDS" [
-									mode: 'dmards ;'
-								] [
-									append medications line
-								]
+						] [
+							if all [mode = 'medication not empty? medications] [
+								print "empty line, in medication mode, and not empty medications"
+								mode: 'finish
 							]
-
-							dmards [
-								append dmards line
-							]
-
-							finish [
-								print "break out of lines as no diagnosis or medications in this letter"
-								break
-							]
+							if all [mode = 'dmards not empty? dmards] [mode: 'finish]
 						]
-
-					] [
-						if all [mode = 'medication not empty? medications] [
-							print "empty line, in medication mode, and not empty medications"
-							mode: 'finish
-						]
-						if all [mode = 'dmards not empty? dmards] [mode: 'finish]
 					]
-				]
-				?? mode
-				?? longdate
-				?? nhi
-				?? surname
-				?? fname
-				?? sname
-				?? address
-				?? areacode
-				?? mobile
-				?? phone
-				?? email
-				?? fp
-				?? current-doc
-				?? fpaddress
-				?? medications
-				?? diagnoses
-				?? diagnosis-detail
-				?? dmards
-				; ++ cnt
-				; if cnt > 100 [halt]
-				print "================================================="
-				][
+					?? mode
+					?? longdate
+					?? nhi
+					?? surname
+					?? fname
+					?? sname
+					?? address
+					?? areacode
+					?? mobile
+					?? phone
+					?? email
+					?? fp
+					?? current-doc
+					?? fpaddress
+					?? medications
+					?? diagnoses
+					?? diagnosis-detail
+					?? dmards
+					; ++ cnt
+					; if cnt > 100 [halt]
+					print "================================================="
+				] [
 					print "letter already in database"
 				]
 			] [
