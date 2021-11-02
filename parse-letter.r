aa778be4bca5906bc03befc2c6a4532762060e41
Graham Chiu <compkarori@gmail.com>
2021-11-02 03:25:02 +0000

    Update parse-letter.r

--- parse-letter.r
+++ parse-letter.r
@@ -24,6 +24,7 @@ areacode-rule: [4 digit]
 dob-rule: [2 digit "." 2 digit "." 4 digit]
 alpha: charset [#"a" - #"z" #"A" - #"Z"]
 name-rule: charset [#"a" - #"z" #"A" - #"Z" #"-" #"'" #" "]
+fname-rule:[some alpha #"-" some alpha | some alpha]
 uc: charset [#"A" - #"Z"]
 nhi-rule: [3 alpha 4 digit]
 filename-rule: [nhi-rule "-" some alpha "-202" 5 digit "-" digit ".txt"]
@@ -77,7 +78,7 @@ foreach record copy port [
 
 						name [;look for patient name next eg. XXXX, XXXX XXXX or XXX XXX, XXX XXX
 							?? line
-							either parse/all line [uc some name-rule ", " copy fname some alpha opt [" " copy sname to end] end] [
+							either parse/all line [uc some name-rule ", " copy fname fname-rule opt [" " copy sname to end] end] [
 								; we have surnames, and first names
 								parse/all line [copy surname to ","]
 								?? surname ?? fname ?? sname
