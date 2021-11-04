a37a68887298413367cfb58031f76c4756d2e54b
Graham Chiu <compkarori@gmail.com>
2021-11-04 02:02:49 +0000

    Update generate-lists.r

--- generate-lists.r
+++ generate-lists.r
@@ -3,20 +3,19 @@ Rebol [
 
 ]
 
-patients: []
 ; NHI Clinic-Date First-Name Surname Street1 Street2 Town Drug1 Dose1 Drug2 Dose2
 
 dbase: open odbc://patients
 port: first dbase
 
-patients: copy []
+patient-ids: copy []
 
 biologics: ["Updacit" "Enbrel" "Etanercept" "Humira" "Adalimumab" "Rituximab" "Secukinumab" "Cosentyx" "Infliximab" "Remicade" "Tocilizumab"]
 
 foreach biologic biologics [
     insert port [{select nhi, letter from medications where name like (?) and active = 'T'} join biologic "%"]
     foreach record copy port [
-        append/only patients record
+        append/only patients-ids record
     ]
 ]
 
