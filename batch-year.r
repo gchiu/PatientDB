77eb48d52b8e964f7fe32a17a53b40652b9328ba
Graham Chiu <compkarori@gmail.com>
2021-11-04 05:01:09 +0000

    Update batch-year.r

--- batch-year.r
+++ batch-year.r
@@ -3,22 +3,22 @@ Rebol [
 ]
 
 y2021: ["October" "September" "August" "July" "June" "May" "April" "March" "February" "January"]
-year:  ["December" "November" "October" "September" "August" "July" "June" "May" "April" "March" "February" "January"]
+years:  ["December" "November" "October" "September" "August" "July" "June" "May" "April" "March" "February" "January"]
 
-foreach month y2021 [
-	dir: rejoin [%2021/2021/ month "/"]
+foreach cmonth y2021 [
+	dir: rejoin [%2021/2021/ cmonth "/"]
 	do %read-files.r
 	do %parse-letter.r 
 ]
 
-foreach month year [
-	dir: rejoin [%2020/2020/ month "/"]
+foreach cmonth years [
+	dir: rejoin [%2020/2020/ cmonth "/"]
 	do %read-files.r
 	do %parse-letter.r 
 ]
 
-foreach month year [
-	dir: rejoin [%2019/2019/ month "/"]
+foreach cmonth years [
+	dir: rejoin [%2019/2019/ cmonth "/"]
 ?? dir
 	do %read-files.r
 	do %parse-letter.r 
