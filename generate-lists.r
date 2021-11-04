e5feb4a5b923e38dd3cda2b767ee5eecebe5f738
Graham Chiu <compkarori@gmail.com>
2021-11-04 02:22:17 +0000

    Update generate-lists.r

--- generate-lists.r
+++ generate-lists.r
@@ -10,7 +10,7 @@ port: first dbase
 
 patient-ids: copy []
 
-biologics: ["Updacit" "Enbrel" "Etanercept" "Humira" "Adalimumab" "Rituximab" "Secukinumab" "Cosentyx" "Infliximab" "Remicade" "Tocilizumab"]
+biologics: ["Updacit" "Rinvoq" "Enbrel" "Etanercept" "Humira" "Adalimumab" "Rituximab" "Secukinumab" "Cosentyx" "Infliximab" "Remicade" "Tocilizumab"]
 
 foreach drug biologics [
     insert port [{select nhi, letter from medications where name like (?) and active = 'T'} join drug "%"]
@@ -29,3 +29,16 @@ foreach drug immunos [
         append/only patient-ids record
     ]
 ]
+
+; now we need to generate ; NHI Clinic-Date First-Name Surname Street1 Street2 Town Drug1 Dose1 Drug2 Dose2
+; patient-ids [ [id clinicdate medication]]
+
+patients: copy []
+
+foreach record patient-ids [
+    insert port [{select (fname, surname, street, street2, town) from patients where nhi =(?) record/1}]
+    rec: pick port 1
+    append rec [record/2 record/3]
+    append/only patients rec
+]
+
